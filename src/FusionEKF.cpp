2affdb166c0d5b4d4ba497a468930943321e8fd8
kzon <kzon@nexway.com>
2017-08-07 14:30:42 +0200

    Change in R_radar that made it working for dataset 1

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -29,7 +29,7 @@ FusionEKF::FusionEKF() {
 
   //measurement covariance matrix - radar
   R_radar_ << 0.09, 0, 0,
-        0, 0.0009, 0,
+        0, 0.1, 0,
         0, 0, 0.09;
 
   // initialized measurement matrix
